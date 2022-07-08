09c9ec0c97bd1e4627cc3f117906790302d25c00
funneld <funneld@gmail.com>
2022-07-08 08:35:39 +0200

    replace sleep with timer

--- jni/auto_start_recording.c
+++ jni/auto_start_recording.c
@@ -3,15 +3,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 #include "auto_start_recording.h"
 
 
-int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {	
-		
+int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
+
 	int32_t ret = 0;
-	
+
 	if (!timeOut && !getInstance){
-		
+
 		timeOut = dlsym (RTLD_NEXT, "_ZN19GlassRacingChnlMenu7timeOutEv");
 		if (timeOut == 0){
 			guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);
@@ -22,7 +23,7 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 				return 0;
 			}
 		}
-		
+
 		getInstance = dlsym (RTLD_NEXT, "_ZN17GlassUserSettings11getInstanceEv");
 		if (getInstance == 0){
 			if(!guiLib){guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);}
@@ -34,20 +35,26 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 			}
 		}
 	}
-	
+
 	uint32_t inst = getInstance();
 
 	gs_link_stat_t link = GS_LINK_STAT_UKNOWN;
-	gs_link_stat_t *link_state = &link;	
-	
+	gs_link_stat_t *link_state = &link;
+
 	uint32_t *gs_gui_config = (uint32_t *)*(uint32_t *)((int)inst + 0xe4);
 	uint32_t *gs_info = (uint32_t *)*(uint32_t *)((int)gs_gui_config + 0x4c);
 	gs_modem_get_link_state_wrap = (void *)*(int32_t *)((int)gs_gui_config + 0x228);
 	gs_lv_transcode_record = (void *)*(int32_t *)((int)gs_gui_config + 0x3D8);
-	
+
 	gs_modem_get_link_state_wrap(gs_info, link_state);
-		
-	if(((link == GS_LINK_STAT_NORMAL) || (link == GS_LINK_STAT_WEAK)) && restart_record){
+
+	if(link != GS_LINK_STAT_NORMAL){
+		clock_gettime(CLOCK_MONOTONIC, &start);
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &now);
+
+	if((link == GS_LINK_STAT_NORMAL) && restart_record && ((now.tv_sec - start.tv_sec) > 9)){
 		restart_record = false;
 		gs_lv_transcode_record(gs_info, true, RECORD_BUTTON);
 		printf("START RECORDING...\n");
@@ -55,10 +62,10 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 	else if(((link == GS_LINK_STAT_LOST) || (link == GS_LINK_STAT_UKNOWN)) && !restart_record){
 		restart_record = true;
 		printf("LINK LOST!\n");
-	} 
+	}
 
-	
-	ret = timeOut(this);		
 
-	return ret;	
+	ret = timeOut(this);
+
+	return ret;
 }
--- jni/auto_start_recording.h
+++ jni/auto_start_recording.h
@@ -1,3 +1,4 @@
+#include <time.h>
 
 typedef enum gs_link_stat_t {
     GS_LINK_STAT_NORMAL=0,
@@ -17,3 +18,4 @@ static int32_t (* gs_modem_get_link_state_wrap)(void *_gs_info, enum gs_link_sta
 static int32_t (* gs_lv_transcode_record)(void *_gs_info, bool b_record, Record_sender sender) = 0;
 static uint32_t (* timeOut)(void *this) = 0;
 static uint32_t (* getInstance)() = 0;
+struct timespec start, now;
