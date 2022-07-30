196ce69682ec977a9ca71e6e813615940954be4d
funneld <funneld@gmail.com>
2022-07-30 21:20:56 +0200

    restart after a disarm

--- jni/auto_start_recording.c
+++ jni/auto_start_recording.c
@@ -6,15 +6,13 @@
 #include "auto_start_recording.h"
 
 
-int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {	
-		
-	int32_t ret = 0;
-	
+void _ZN19GlassRacingChnlMenu7timeOutEv(void* this) {
+
 	if (!timeOut && !getInstance){
-		
+
 		timeOut = dlsym (RTLD_NEXT, "_ZN19GlassRacingChnlMenu7timeOutEv");
 		if (timeOut == 0){
-			guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);
+			if(!guiLib){guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);}
 			timeOut = dlsym (guiLib, "_ZN19GlassRacingChnlMenu7timeOutEv");
 			if (timeOut == 0)
 			{
@@ -22,7 +20,7 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 				return 0;
 			}
 		}
-		
+
 		getInstance = dlsym (RTLD_NEXT, "_ZN17GlassUserSettings11getInstanceEv");
 		if (getInstance == 0){
 			if(!guiLib){guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);}
@@ -34,31 +32,47 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 			}
 		}
 	}
-	
-	uint32_t inst = getInstance();
 
-	gs_link_stat_t link = GS_LINK_STAT_UKNOWN;
-	gs_link_stat_t *link_state = &link;	
-	
-	uint32_t *gs_gui_config = (uint32_t *)*(uint32_t *)((int)inst + 0xe4);
-	uint32_t *gs_info = (uint32_t *)*(uint32_t *)((int)gs_gui_config + 0x4c);
-	gs_modem_get_link_state_wrap = (void *)*(int32_t *)((int)gs_gui_config + 0x228);
-	gs_lv_transcode_record = (void *)*(int32_t *)((int)gs_gui_config + 0x3D8);
-	
-	gs_modem_get_link_state_wrap(gs_info, link_state);
-		
-	if(((link == GS_LINK_STAT_NORMAL) || (link == GS_LINK_STAT_WEAK)) && restart_record){
+	instance = getInstance();
+
+	gs_gui_config = (__gs_gui_config *)*(uint32_t *)((int)instance + 0xe4);
+
+	gs_link_stat_t link_state = GS_LINK_STAT_UKNOWN;
+	gs_link_stat_t *ptr_link_state = &link_state;
+	record_state_t record_state;
+
+	gs_gui_config->gs_get_uav_power_status(gs_gui_config->gs_info, ptr_power_status, ptr_arm_flag);
+	gs_gui_config->gs_modem_get_link_state_wrap(gs_gui_config->gs_info, ptr_link_state);
+
+
+	if((link_state == GS_LINK_STAT_NORMAL) && restart_record) {
 		restart_record = false;
-		gs_lv_transcode_record(gs_info, true, RECORD_BUTTON);
+		gs_gui_config->gs_lv_transcode_record(gs_gui_config->gs_info, true, RECORD_BUTTON);
 		printf("START RECORDING...\n");
 	}
-	else if(((link == GS_LINK_STAT_LOST) || (link == GS_LINK_STAT_UKNOWN)) && !restart_record){
+	else if((link_state == GS_LINK_STAT_LOST) && !restart_record){
 		restart_record = true;
 		printf("LINK LOST!\n");
-	} 
+	}
 
-	
-	ret = timeOut(this);		
+	if((prev_arm_flag - arm_flag) == 1){
+		int cnt = 0;
+		while(cnt < 20){ //check rec state for max 2 seconds
+			cnt += 1;
+			record_state = gs_gui_config->gs_lv_transcode_get_state(gs_gui_config->gs_info);
+			if(record_state != RECORD_STATE_RECORDING){
+				gs_gui_config->gs_lv_transcode_record(gs_gui_config->gs_info, true, RECORD_BUTTON);
+				printf("RESTART RECORDING AFTER A DISARM...\n");
+				break;
+			}
+			usleep(100000);
+		}
+	}
 
-	return ret;	
+	prev_arm_flag = arm_flag;
+
+
+	timeOut(this);
+
+	return;
 }
